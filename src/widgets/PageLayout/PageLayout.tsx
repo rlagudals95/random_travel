6ef187ec5560097a58136c3c3c5af588c1e6965f
hmk2024 <hm.kim@moyoplan.com>
2025-01-18 19:02:10 +0900

    feat: remove background color

--- src/widgets/PageLayout/PageLayout.tsx
+++ src/widgets/PageLayout/PageLayout.tsx
@@ -5,7 +5,7 @@ export default function PageLayout({
     children: React.ReactNode;
   }>) {
     return (
-    <div className="min-h-screen w-full max-w-[430px] mx-auto bg-gradient-to-b from-blue-200 to-blue-50 dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-screen w-full max-w-[430px] mx-auto">
         {children}
     </div>
 );
