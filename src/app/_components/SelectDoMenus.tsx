280cac36b3f372cf0e3ec16035c6f0b5e4c86ed1
hmk2024 <hm.kim@moyoplan.com>
2025-01-18 17:51:12 +0900

    feat: kakao key setting

--- src/app/_components/SelectDoMenus.tsx
+++ src/app/_components/SelectDoMenus.tsx
@@ -8,7 +8,7 @@ export const SelectDoMenus = () => {
       {(['all', ...(DO_MAP.keys())]).map((region) => {
         return (
           <Link key={region} href={`/map-page/${region === 'all' ? 'all' : DO_MAP.get(region as Do)}`} className="w-full hover:bg-gray-100 rounded-lg p-4 bg-gray-50 text-center font-bold">
-            <div className="text-lg font-bold">
+            <div className="text-lg font-bold text-gray-800">
               {region === 'all' ? '전국' : region}
             </div>
           </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { CopyLinkButton } from "@/features/share/ui/CopyLinkButton";
 export default function Home() {
 
   return (
-    <div className="min-h-screen w-full max-w-[430px] mx-auto bg-gradient-to-b from-blue-200 to-blue-50 dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-screen w-full max-w-[430px] mx-auto bg-blue-100">
       <Header />
       <main className="flex flex-col items-center h-full">
         <SelectDoMenus />
